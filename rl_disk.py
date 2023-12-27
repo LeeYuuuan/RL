9189c2bdb42b384182e2e4db289a86b7563fea06
LeeYuuuan <LeeYuuuan@outlook.com>
2023-12-27 12:10:32 +0800

    end

--- rl_disk.py
+++ rl_disk.py
@@ -2,6 +2,7 @@ from typing import Union
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.patches import Circle
+import random
 
 
 
@@ -67,6 +68,21 @@ class Functions:
         return (point_1.get_x() - point_2.get_x()) ** 2 + (point_1.get_y() - point_2.get_y()) **2
     
     @staticmethod
+    def action_to_del_coordinate(a):
+        """ 0 for up, 1 for down, 2 for left, 3 for rigth"""
+        if a == 0:
+            return [0, 1]
+        if a == 1:
+            return [0, -1]
+        if a == 2:
+            return [-1, 0]
+        if a == 3:
+            return [1, 0]
+        else:
+            return False
+    
+    
+    @staticmethod
     def show_disk(current_center: Point, radius):
         
         disk = plt.Circle((current_center.get_x(),current_center.get_y()),radius)
@@ -121,7 +137,7 @@ class Functions:
 
 # show_point(load_point())
 class Square:
-    def __init__(self, side_len=4, divided_len=1, radius=1.2):
+    def __init__(self, side_len=4, divided_len=1, radius=1.2, gamma=0.8):
         self.side_len = side_len
         self.divided_len = divided_len
         self.radius = radius
@@ -132,7 +148,10 @@ class Square:
         self.load_points()
 
         # q-learning
-        self.one_disk_states = None
+        self.gamma = gamma
+        self.one_disk_r = None
+        self.state_size_one_disk = len(self.intersections) + 1 # s= 25: "end" state.
+        self.one_disk_Q = np.zeros([self.state_size_one_disk, 5])
         
     
     def load_intersections(self):
@@ -175,15 +194,44 @@ class Square:
         print(number_points_list)
         max_value = max(number_points_list)
         max_index = number_points_list.index(max_value)
-        max_intersection = self.intersections[max_index]
+        # max_intersection = self.intersections[max_index]
+        # print(len(number_points_list))
         return max_index
     
 
     """For Q-learning"""
-    def initialize_one_disk_states():
-        raise NotImplementedError
-    
-    def Q(self, s, a):
+    def coordi_is_legal(self, coordi):
+        
+        for c in coordi:
+            if c not in range(0, self.side_len+1):
+                return False
+        return True
+                
+
+    def state_to_new(self, s, a):
+        """ 
+        0 for up,
+        1 for down, 
+        2 for left, 
+        3 for rigth
+        4 for end"""
+        if s in range(self.state_size_one_disk) and a in range(5):
+            if a == 4 or s == 25:
+                return 25
+            if a == 0 and s % 5 != 4:
+                return s + 1
+            if a == 1 and s % 5 != 0:
+                return s - 1
+            if a == 2 and s not in range(0, self.side_len+1):
+                return s - self.side_len - 1
+            if a == 3 and s not in range(self.state_size_one_disk-self.side_len-1, self.state_size_one_disk):
+                return s + self.side_len + 1    
+            
+        
+        return -1
+
+
+    def calculate_r_one_disk(self, s, a):
         """calculate Q.
 
         Args:
@@ -191,11 +239,79 @@ class Square:
             a (int): action, 0, 1, 2, 3: for Up, down, left, right
         
         Return:
-            float: return Q value.
+            float: return r value.
         """
-        res = 0
-        return res 
+        if s in range(self.state_size_one_disk) and a in range(5):
+            new_state = self.state_to_new(s, a)
+            if new_state == 25:
+                return 0
+            
+            if new_state != -1:
+                return self.number_points_in_disk(self.intersections[new_state]) - self.number_points_in_disk(self.intersections[s])
+
+        return -100
     
+    def initialize_one_disk_r(self):
+        """ 
+        initialize reward matrix for one disk
+        state * action = 26 * 5 (25 + end, 4 + end)
+        """
+        r_mtx = np.zeros([self.state_size_one_disk, 5])
+        for i in range(self.state_size_one_disk):
+            for j in range(5):
+                r_mtx[i, j] = self.calculate_r_one_disk(i, j)
+        
+        self.one_disk_r = r_mtx
+    
+    def update_one_disk_Q(self, s, a):
+        self.one_disk_Q[s, a] = self.one_disk_r[s, a] + self.gamma * self.one_disk_Q[s].max()
+        
+    def one_disk_Q_learning(self):
+        self.initialize_one_disk_r()
+        actions = []
+        for i in range(1000):
+            s = random.randint(0, 24)
+            for action in range(5):
+                if self.one_disk_r[s, action] > -50:
+                    actions.append(action)
+            a = actions[random.randint(0, len(actions) - 1)]
+            self.update_one_disk_Q(s, a)
+        print(self.one_disk_Q)
+
+    
+    def one_disk_find_max(self):
+        self.one_disk_Q_learning()
+        state = 13
+        print(f"the disk is on state:{state}")
+        count = 0
+        states = []
+        while state != 25:
+            states.append(state)
+            if count > 20:
+                print("fail")
+                break
+
+            q_max = self.one_disk_Q[state].max()
+
+
+            q_max_action = np.argmax(self.one_disk_Q[state])
+            
+            new_state = self.state_to_new(state, q_max_action)
+
+            if new_state != -1:
+                state = new_state
+            else:
+                print("fail state!")
+                break
+            print(f"the disk move to state{state}")
+        max_index = states[-1]
+        Functions.show_coverd_state(self.intersections[max_index], self.radius, self.points)
+        # plt.imshow(self.one_disk_Q ,cmap='hot',interpolation='nearest')
+        # plt.show()
+            
+
+
+
     def test_block(self):
         test_point = self.points[0]
         test_intersection = self.intersections[1]
@@ -203,12 +319,21 @@ class Square:
         # print(self.number_points_in_disk(test_intersection))
         # Functions.show_coverd_state([test_intersection, self.intersections[2]], self.radius, self.points)
         max_index = self.hard_code_implementation()
-        Functions.show_coverd_state(self.intersections[max_index], self.radius, self.points)
+        # Functions.show_coverd_state(self.intersections[max_index], self.radius, self.points)
+        # print(self.Q_one_disk(5, 3))
+        self. initialize_one_disk_r()
+        print(self.one_disk_r)
+        print(self.one_disk_Q[0].max())
+
         
             
 
 square = Square()
 # Functions.show_point(square.points)
-square.test_block()
+# square.test_block()
 # square.hard_code_implementation()
+# square.one_disk_Q_learning()
+square.one_disk_find_max()
+
+
 
